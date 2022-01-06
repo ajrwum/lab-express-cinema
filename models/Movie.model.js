b0a28533b77e26bfd666e6d9c54bd479ce9b0d88
Ajr Wum <ajr.wum@gmail.com>
2022-01-06 16:19:43 +0100

    iteration 1 finished - create seeding script and shortcut in package.json

---
+++ models/Movie.model.js
@@ -0,0 +1,18 @@
+// loading elements from library
+const { model, Schema } = require('mongoose');
+
+// creating Schema (from observing the data)
+const movieSchema = new Schema({
+  title: String,
+  director: String,
+  stars: [ String ],
+  image: String,
+  description: String,
+  showTimes: [ String ]
+});
+
+// creating the db model for movies
+const Movie = model("Movie", movieSchema);
+
+// exporting to be able to access it when necessary
+module.exports = Movie;
